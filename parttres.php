2871aa449bd6176cd5c8f49906d28ec7f56eada8
Ryan Z. Costa <ryanzanec@gmail.com>
2016-06-28 19:34:15 -0400

    Update parttres.php

--- parttres.php
+++ parttres.php
@@ -1,4 +1,13 @@
 <?php session_start(); ?>
+<!-- 
+   #######################################################################################################
+   || This code was written by Ryan Z. Costa in June 2016 as a practice and exercise in HTML & Css & PHP||
+   || This is purely for educational purposes and the images used in this page are not mine.            ||  
+   || This is not for business purposes, only personal use and educational practice.                    ||  
+   || Pokemon, its characters, and everything pokemon related belongs to nintendo/gamefreak             ||
+   #######################################################################################################
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 
